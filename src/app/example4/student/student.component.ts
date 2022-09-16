b280c6a9654ecbde4e33bfc692832ff05bfc35c3
Ogi62 <danguba962@gmail.com>
2022-09-16 13:49:10 +0200

    some chanes in example 4

--- src/app/example4/student/student.component.ts
+++ src/app/example4/student/student.component.ts
@@ -1,22 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { StudentInterface } from '../models/Student';
 
-
 @Component({
-    selector: 'student',
-    templateUrl: './student.component.html',
-    styleUrls: ['./student.component.scss']
+  selector: 'student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss'],
 })
-
 export class Student {
-    @Output() newStudent = new EventEmitter<StudentInterface>();
+  @Output() newStudent = new EventEmitter<StudentInterface>();
 
-
-    addStudent(student: string) {
-        const studentObj : StudentInterface = {
-            id: Math.floor(Math.random() * 1999999),
-            name: student
-        };
-        this.newStudent.emit(studentObj);
+  addStudent(student: HTMLInputElement) {
+    if (!student.value) {
+      return;
     }
+    const studentObj: StudentInterface = {
+      id: this.makeId(),
+      name: student.value,
+    };
+    this.newStudent.emit(studentObj);
+    student.value = '';
+  }
+
+  private makeId() {
+    return Math.floor(Math.random() * 1000000);
+  }
 }
