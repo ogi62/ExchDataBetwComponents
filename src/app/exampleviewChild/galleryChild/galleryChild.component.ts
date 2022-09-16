d341c5bf1ae7e15682e9fc447934e011cc1876d7
Ogi62 <danguba962@gmail.com>
2022-09-16 15:15:02 +0200

    added method for gen img and displayed 5 images in gal compo

--- src/app/exampleviewChild/galleryChild/galleryChild.component.ts
+++ src/app/exampleviewChild/galleryChild/galleryChild.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'gallery-child',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
     styleUrls: ['./galleryChild.component.scss']
 })
 
-export class GalleryChildComponent {
-    
+export class GalleryChildComponent implements OnInit {
+    pictures: string[] = [];
+
+    constructor() {}
+
+    ngOnInit() {
+        this.pictures = new Array(5).fill(0).map(this.generateImg);
+    }
+
+    generateImg() {
+        return `https://picsum.photos/200/200?ts${Math.random()*30 + 1}`;
+    }
 }
