d880c1571bd8457e8fb23dd2f0adb344b9c903b7
Ogi62 <danguba962@gmail.com>
2022-09-16 16:20:15 +0200

    viewChild implemented in parentApp component with add and delete img methods

--- src/app/exampleviewChild/parentApp/parentApp.component.ts
+++ src/app/exampleviewChild/parentApp/parentApp.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GalleryChildComponent } from '../galleryChild/galleryChild.component';
 
 @Component({
-    selector: 'parent-app',
-    templateUrl: './parentApp.component.html',
-    styleUrls: ['./parentApp.component.scss']
+  selector: 'parent-app',
+  templateUrl: './parentApp.component.html',
+  styleUrls: ['./parentApp.component.scss'],
 })
-
 export class ParentAppComponent {
+  @ViewChild(GalleryChildComponent) gallery;
 
+  constructor() {}
+
+  addImg() {
+    if (this.gallery.pictures.length === 10) {
+      alert('to many images');
+      return;
+    }
+    const img = this.gallery.generateImg();
+    this.gallery.pictures.unshift(img);
+  }
+
+  removeFirstImg() {
+    if (this.gallery.pictures.length === 0) {
+      alert('no images');
+      return;
+    }
+
+    this.gallery.pictures.shift();
+  }
 }
