9748b3f8b8d20c1ae72fd2a7cc238928bf48d2ea
Ogi62 <danguba962@gmail.com>
2022-09-23 12:51:46 +0200

    made example with btn and modal with true/false and with viewChild

--- src/app/exampleviewChild/buttonExample/button-example/button-example.component.ts
+++ src/app/exampleviewChild/buttonExample/button-example/button-example.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'button-example',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-example.component.css']
 })
 export class ButtonExampleComponent implements OnInit {
+  showModal = false;
+  @ViewChild('mainBtn') modalEl: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openModal() {
+    this.showModal = true;
+    this.modalEl.nativeElement.disabled = true;
+  }
+
+  closeModal() {
+    this.showModal= false;
+    this.modalEl.nativeElement.disabled = false;
+  }
+
+  hideBtn() {
+    console.log(this.modalEl)
+    this.modalEl.nativeElement.hidden = true;
+    this.showModal = false;
   }
 
 }
