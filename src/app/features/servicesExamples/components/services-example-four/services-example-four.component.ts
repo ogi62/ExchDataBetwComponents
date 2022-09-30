1780f12b50fa8ad7694e417a1c9b0f09f986bc8e
Ogi62 <danguba962@gmail.com>
2022-09-30 22:57:15 +0200

    manage to display courses with course service

--- src/app/features/servicesExamples/components/services-example-four/services-example-four.component.ts
+++ src/app/features/servicesExamples/components/services-example-four/services-example-four.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Course } from 'src/app/shared/models/course.model';
+import { CoursesService } from '../../services/exampleFourService/courses.service';
 
 @Component({
   selector: 'app-services-example-four',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesExampleFourComponent implements OnInit {
 
-  constructor() { }
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses() {
+    const courses$ = this.coursesService.loadAllCourses();
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses=> courses.filter(course => course.category == 'BEGINNER'))
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map(courses=> courses.filter(course => course.category == 'ADVANCED'))
+    );
   }
 
 }
