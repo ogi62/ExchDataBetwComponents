8d868f8e4fc165a3ccee31bfb8e1fbb0675d364e
Ogi62 <danguba962@gmail.com>
2022-10-08 22:55:43 +0200

    manage to fetch books and to style the books

--- src/app/features/servicesExamples/components/services-example-five/components/all-books/all-books.component.ts
+++ src/app/features/servicesExamples/components/services-example-five/components/all-books/all-books.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-all-books',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllBooksComponent implements OnInit {
 
-  constructor() { }
+  private apiUrl = 'https://hapi-books.p.rapidapi.com/month/2022/3';
+  private options = {
+    method: 'GET',
+    url: 'https://hapi-books.p.rapidapi.com/month/2022/3',
+    headers: {
+      'X-RapidAPI-Key': '959ab1ba7dmshb9b7f55aa2ac655p17197bjsn1f7efe207b54',
+      'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+    }
+  };
+
+    books$: Observable<any>;
+
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': '959ab1ba7dmshb9b7f55aa2ac655p17197bjsn1f7efe207b54',
+  //     'X-RapidAPI-Host': 'books17.p.rapidapi.com'
+  //   }
+  // };
+  
+  // fetch('https://books17.p.rapidapi.com/authors/8418015', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+   this.books$ =  this.allBooks();
+  }
+
+  allBooks(): Observable<any> {
+    return this.http.get(this.apiUrl,this.options)
   }
 
 }
